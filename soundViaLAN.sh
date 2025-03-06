388f2acb53daa8eb4fdd01b80ad2aca3310a5412
Astramast <azerqsdfazerqsdg@gmail.com>
2025-03-06 01:51:29 +0100

    updated soundViaLAN

--- soundViaLAN.sh
+++ soundViaLAN.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo apt update && sudo apt install pulseaudio -y
-echo "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1;172.23.100.0/24" | sudo tee -a /etc/pulse/default.pa
+sudo apt update && sudo apt install pulseaudio pulseaudio-module-zeroconf -y
+echo "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1;172.23.100.0/24" | sudo tee -a /etc/pulse/default.pa.d/soundViaLAN.pa
+echo "load-module module-zeroconf-publish" | sudo tee -a /etc/pulse/default.pa.d/soundViaLAN.pa
 
