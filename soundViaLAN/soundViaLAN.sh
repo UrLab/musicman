0612e3e47116bce683a84ca9f3dc8de15000108f
Astramast <azerqsdfazerqsdg@gmail.com>
2025-04-04 20:38:43 +0200

    Fri  4 Apr 20:38:43 CEST 2025

--- soundViaLAN/soundViaLAN.sh
+++ soundViaLAN/soundViaLAN.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt install pulseaudio pulseaudio-module-zeroconf -y
 echo "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1;172.23.100.0/24" | sudo tee -a /etc/pulse/system.pa.d/soundViaLAN.pa
 echo "load-module module-zeroconf-publish" | sudo tee -a /etc/pulse/system.pa.d/soundViaLAN.pa
-sudo cp pulseaudio.service /etc/systemd/system/pulseaudio.service
+sudo ln -s $(realpath pulseaudio.service) /etc/systemd/system/pulseaudio.service
 systemctl --user mask pulseaudio.socket
 systemctl --user disable pulseaudio.service
 sudo systemctl enable --now pulseaudio
