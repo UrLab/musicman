ca8c6f61aaecff8a0f5d7af01205de7b2456a28e
Tsunahhh <malfroidthomas@gmail.com>
2025-09-23 21:29:50 +0200

    Python class to download music with settings.

---
+++ UrSongDownloader/downloader.py
@@ -0,0 +1,27 @@
+import json
+import yt_dlp
+
+class Downloader:
+
+    def __init__(self, prefered_format="opus", out_path: str="."):
+        self.prefered_format = prefered_format
+        self.out_path = out_path
+        
+    def download(self, url: str, codec="opus", path="./audio", max_size="128"):
+        ydl_opts = {
+            'format': f'bestaudio[abr<={max_size}]',
+            'outmpl': '%(title)s.%(ext)s',
+            'postprocessors': [{  
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': f'{codec}',
+            }]
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            error_code = ydl.download(url)
+
+# If you want to try without the web interface
+if __name__ == "__main__":
+    pass
+    #dld = UrDownloader()
+    #dld.download("https://www.youtube.com/watch?v=Ktv5dIBlvQ8", "opus", ".")
