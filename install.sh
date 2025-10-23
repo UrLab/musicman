059e1272b55f409932ac7102ef84f99a505f7478
Astramast <azerqsdfazerqsdg@gmail.com>
2025-10-23 22:55:07 +0200

    made that both bard and ursd write and read to the same folder

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-cd bard
 mkdir -p music
+cd bard
 docker-compose up --build -d
 cd ../soundViaLAN
 ./soundViaLAN.sh
 cd ../UrSongDownloader
-mkdir -p music
 docker-compose up --build -d
 
